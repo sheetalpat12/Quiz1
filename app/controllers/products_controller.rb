8a717ea2a101b81e77bf35b001c1450b9acd759a
sheetal.sompura1985@gmail.com <sheetal.sompura1985@gmail.com>
2019-01-16 04:20:02 +0000

    added a new form to enter details for the product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,20 @@ class ProductsController < ApplicationController
     def new
       @product = Product.new
     end
+
+
+    def create
+      @product = Product.create(product_params)
+
+        if @product.invalid?
+            flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+        end
+        redirect_to root_path
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :description)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
-  
+  validates :name, presence: true, length: { maximum: 40, minimum: 3 }
+  validates :description, presence: true, length: { maximum: 150, minimum: 3 }
 end
